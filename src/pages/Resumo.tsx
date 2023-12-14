0a708393e81244db4931c2d866968625d0e5edca
Matheus <matheustorresdev@gmail.com>
2023-12-14 20:18:27 -0300

    graficos

--- src/pages/Resumo.tsx
+++ src/pages/Resumo.tsx
@@ -1,3 +1,4 @@
+import GraficoVendas from "../components/GraficoVendas";
 import { useData } from "../hooks/useDataContext";
 
 const Resumo = () => {
@@ -36,7 +37,9 @@ const Resumo = () => {
           </span>
         </div>
       </div>
-      <div className="box mb">Gráficos</div>
+      <div className="box mb">
+        <GraficoVendas data={data}/>
+      </div>
     </section>
   );
 };
